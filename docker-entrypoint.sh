ddf722ed0769d69ef0f19d1e3991be3f4abd64ae
Rafał Masiarek <36776423+rafalmasiarek@users.noreply.github.com>
2022-04-18 21:29:11 +0200

    fix problem with nullglob
    remove shopt

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,32 +1,31 @@
 #!/bin/sh
-# bugs report to 
+# bugs report to
 # Infrastruktura.IT DevOps Team <maintainers-docker@infrastruktura.it>
 
 set -e
 
 if [[ -d /docker-entrypoint.d ]]; then
 
-    shopt -s nullglob
-
-    echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
+    echo "$0: Looking for shell scripts in /docker-entrypoint.d/" >&1
     for file in /docker-entrypoint.d/*; do
+        echo "---> $file"
         case "$file" in
             *.sh)
-                if [ -x "$f" ]; then
-                    echo >&3 "$0: Launching $f";
-                    "$f"
+                if [ -x "$file" ]; then
+                    echo "$0: Launching $file" >&1
+                    "$file"
                 else
-                    # warn on shell scripts without exec bit
-                    echo >&3 "$0: Ignoring $f, not executable";
+                    # err on shell scripts without exec bit
+                    echo "$0: Ignoring $file, not executable" >&2
                 fi
                 ;;
-            *) echo >&3 "$0: Ignoring $f";;
+            *) echo "$0: Ignoring $file" >&2;;
         esac
     done
 
-    echo >&3 "$0: Configuration complete; ready for start up"
+    echo "$0: Configuration complete; ready or start up" >&1
 else
-    echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+    echo "$0: No files found in /docker-entrypoint.d/, skipping configuration" >&1
 fi
 
 exec "$@"
